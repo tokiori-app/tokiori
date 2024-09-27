e072ae6ed0636b8a353413384741a4a5588c5694
Kim Ji Yu <spbabo97@naver.com>
2024-09-28 02:12:41 +0900

    feat : 한자+히라가나 Ruby 컴포넌트

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,28 @@
-import { View, Text } from 'react-native';
+import styled from 'styled-components/native';
 
 const index = () => {
   return (
-    <View>
-      <Text>index</Text>
-    </View>
+    <RubyContainer>
+      {/* 히라가나 */}
+      <RubyText>ふりがな</RubyText>
+      {/* 한자 */}
+      <BaseText>漢字</BaseText>
+    </RubyContainer>
   );
 };
 
 export default index;
+
+const RubyContainer = styled.View`
+  align-items: 'center';
+`;
+
+const RubyText = styled.Text`
+  font-size: 10px;
+  position: absolute;
+  top: -12;
+`;
+
+const BaseText = styled.Text`
+  font-size: 20px;
+`;
