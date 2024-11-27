5f9df5c10f1e8554ea6a4c0fc474bfd06816ab97
Kim Ji Yu <spbabo97@naver.com>
2024-11-27 17:49:13 +0900

    feat : reanimated init

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,10 @@
 /* eslint-env node */
 const { getDefaultConfig } = require('expo/metro-config');
+const {
+  wrapWithReanimatedMetroConfig,
+} = require('react-native-reanimated/metro-config');
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
-module.exports = config;
+module.exports = wrapWithReanimatedMetroConfig(config);
