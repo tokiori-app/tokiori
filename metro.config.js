81ebb71baffc631f93f2d2e269dd8723d3fe17b3
Kim Ji Yu <spbabo97@naver.com>
2024-11-27 18:29:53 +0900

    delete : wrapWithReanimatedMetroConfig
    reanimted에서는 wrapWithReanimatedMetroConfig을 권장하지만 expo에서는 오류가 발생함

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,7 @@
 /* eslint-env node */
 const { getDefaultConfig } = require('expo/metro-config');
-const {
-  wrapWithReanimatedMetroConfig,
-} = require('react-native-reanimated/metro-config');
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
-module.exports = wrapWithReanimatedMetroConfig(config);
+module.exports = config;
