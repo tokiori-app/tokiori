f4dd9feb113ec83fd0f0043219979273687bef00
Kim Ji Yu <spbabo97@naver.com>
2024-11-28 09:26:05 +0900

    feat : const - colors

---
+++ constant/color.ts
@@ -0,0 +1,14 @@
+const COLORS = {
+  main: '#F5888D',
+  secondary: '#FEF3F4',
+  tertiary: '#90B28D',
+  correct: '#2ECC71',
+  incorrect: '#E74C3C',
+  black: '#111111',
+  white: '#FFFFFF',
+  gray1: '#333333',
+  gray2: '#666666',
+  gray3: '#ECECEC',
+};
+
+export default COLORS;
