c9d4e309cbc8ff3437589ae72e3b2b4ca8855b4f
WK <mwk0725@gmail.com>
2024-11-30 17:36:29 +0900

    feat: expo font 적용

---
+++ styles/typography.ts
@@ -0,0 +1,69 @@
+import { StyleSheet } from 'react-native';
+
+const t = StyleSheet.create({
+  // Pretendard 폰트 (한국어)
+  title1: {
+    fontFamily: 'PretendardBold',
+    fontSize: 36,
+    lineHeight: 36 * 1.5,
+    letterSpacing: -0.9,
+  },
+  title2: {
+    fontFamily: 'PretendardBold',
+    fontSize: 16,
+    lineHeight: 16 * 1.5,
+    letterSpacing: -0.4,
+  },
+  heading1: {
+    fontFamily: 'PretendardBold',
+    fontSize: 20,
+    lineHeight: 20 * 1.5,
+    letterSpacing: -0.5,
+  },
+  heading2: {
+    fontFamily: 'PretendardBold',
+    fontSize: 14,
+    lineHeight: 14 * 1.5,
+    letterSpacing: -0.35,
+  },
+  title3: {
+    fontFamily: 'Pretendard',
+    fontSize: 16,
+    lineHeight: 16 * 1.5,
+    letterSpacing: -0.4,
+  },
+  subtitle: {
+    fontFamily: 'Pretendard',
+    fontSize: 14,
+    lineHeight: 14 * 1.5,
+    letterSpacing: -0.35,
+  },
+
+  // Pretendard JP 폰트 (일본어)
+  jpExlarge: {
+    fontFamily: 'PretendardJPBold',
+    fontSize: 44,
+    lineHeight: 44 * 1.5,
+    letterSpacing: 0,
+  },
+  jpLarge: {
+    fontFamily: 'PretendardJPBold',
+    fontSize: 24,
+    lineHeight: 24 * 1.5,
+    letterSpacing: 0,
+  },
+  jpMedium: {
+    fontFamily: 'PretendardJPBold',
+    fontSize: 16,
+    lineHeight: 16 * 1.5,
+    letterSpacing: 0,
+  },
+  jpSmall: {
+    fontFamily: 'PretendardJP',
+    fontSize: 14,
+    lineHeight: 14 * 1.3,
+    letterSpacing: 0,
+  },
+});
+
+export default t;
