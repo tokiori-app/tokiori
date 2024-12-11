31f3d1dc183d495ccd2b28668e89c7b38ef97faf
Kim Ji Yu <spbabo97@naver.com>
2024-12-12 01:36:53 +0900

    feat : prevBtn onClick 함수 추가

--- components/common/layout/Header/subcomponents/PrevBtn.tsx
+++ components/common/layout/Header/subcomponents/PrevBtn.tsx
@@ -5,16 +5,18 @@ import { useRouter } from 'expo-router';
 
 interface PrevBtnProps {
   isClose?: boolean;
+  onClick?: () => void;
 }
 
 /**
  * 뒤로 가기 버튼 | X 버튼
  * @param {boolean | undefined} isClose // < 모양을 x로 바꿉니다.
+ * @param {Function} onClick // 기존 router를 무시하고 onClick 함수를 전달합니다.
  */
-const PrevBtn = ({ isClose }: PrevBtnProps) => {
+const PrevBtn = ({ isClose, onClick }: PrevBtnProps) => {
   const router = useRouter();
   return (
-    <Pressable onPress={router.back}>
+    <Pressable onPress={onClick ? onClick : router.back}>
       {isClose ? <CloseSVG width={24} /> : <LeftArrow width={24} />}
     </Pressable>
   );
