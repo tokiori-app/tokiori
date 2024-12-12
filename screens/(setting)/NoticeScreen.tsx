fdac9ccb24bb3252e7d690b686724281a8fcd75c
Kim Ji Yu <spbabo97@naver.com>
2024-12-13 01:36:52 +0900

    feat : supbabase 연결

--- screens/(setting)/NoticeScreen.tsx
+++ screens/(setting)/NoticeScreen.tsx
@@ -1,7 +1,37 @@
+import { View } from 'react-native';
+import { supabase } from 'lib/supabase';
+import { useEffect, useState } from 'react';
 import DropDown from '@components/page/setting/DropDown';
 
+interface NoticeType {
+  id: number;
+  title: string;
+  content: string;
+  created_at: string;
+}
+
 const NoticeScreen = () => {
-  return <DropDown item={{ content: '' }} index={0} />;
+  const [notices, setNotices] = useState<NoticeType[]>([]);
+
+  useEffect(() => {
+    getNotice();
+  }, []);
+
+  const getNotice = async () => {
+    const { data } = await supabase
+      .from('notices')
+      .select()
+      .returns<NoticeType[]>();
+    if (data) setNotices(data);
+  };
+
+  return (
+    <View>
+      {notices.map((notice, index) => (
+        <DropDown key={index} item={notice} index={index} />
+      ))}
+    </View>
+  );
 };
 
 export default NoticeScreen;
