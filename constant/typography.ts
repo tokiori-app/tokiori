bbc8acfbbfd403019388e2d2960b800b3ee03a17
WK <mwk0725@gmail.com>
2024-12-15 00:52:17 +0900

    feat: 추가된 js 폰트 추가 후 폴더 이동

--- constant/typography.ts
+++ constant/typography.ts
@@ -3,63 +3,69 @@ import { StyleSheet } from 'react-native';
 const t = StyleSheet.create({
   // Pretendard 폰트 (한국어)
   title1: {
-    fontFamily: 'PretendardBold',
+    fontFamily: 'PretendardJPSemiBold',
     fontSize: 36,
     lineHeight: 36 * 1.5,
     letterSpacing: -0.9,
   },
   title2: {
-    fontFamily: 'PretendardBold',
+    fontFamily: 'PretendardJPSemiBold',
     fontSize: 16,
     lineHeight: 16 * 1.5,
     letterSpacing: -0.4,
   },
   heading1: {
-    fontFamily: 'PretendardBold',
+    fontFamily: 'PretendardSemiBold',
     fontSize: 20,
     lineHeight: 20 * 1.5,
     letterSpacing: -0.5,
   },
   heading2: {
-    fontFamily: 'PretendardBold',
+    fontFamily: 'PretendardSemiBold',
     fontSize: 14,
     lineHeight: 14 * 1.5,
     letterSpacing: -0.35,
   },
   title3: {
-    fontFamily: 'Pretendard',
+    fontFamily: 'PretendardMedium',
     fontSize: 16,
     lineHeight: 16 * 1.5,
     letterSpacing: -0.4,
   },
   subtitle: {
-    fontFamily: 'Pretendard',
+    fontFamily: 'PretendardMedium',
     fontSize: 14,
     lineHeight: 14 * 1.5,
     letterSpacing: -0.35,
   },
 
   // Pretendard JP 폰트 (일본어)
-  jpExlarge: {
+  jp60: {
     fontFamily: 'PretendardJPBold',
+    fontSize: 60,
+    lineHeight: 60 * 1.3,
+    letterSpacing: 0,
+  },
+  jp44: {
+    fontFamily: 'PretendardSemiBold',
     fontSize: 44,
     lineHeight: 44 * 1.5,
     letterSpacing: 0,
   },
-  jpLarge: {
-    fontFamily: 'PretendardJPBold',
+  jp24: {
+    fontFamily: 'PretendardSemiBold',
     fontSize: 24,
     lineHeight: 24 * 1.5,
     letterSpacing: 0,
   },
-  jpMedium: {
-    fontFamily: 'PretendardJPBold',
+  jp16: {
+    fontFamily: 'PretendardSemiBold',
     fontSize: 16,
     lineHeight: 16 * 1.5,
     letterSpacing: 0,
   },
-  jpSmall: {
-    fontFamily: 'PretendardJP',
+  jp14: {
+    fontFamily: 'PretendardJPMedium',
     fontSize: 14,
     lineHeight: 14 * 1.3,
     letterSpacing: 0,
