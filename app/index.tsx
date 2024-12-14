8a21ebef25f44d1cff41f5953ca93aff09379bf3
WK <mwk0725@gmail.com>
2024-12-15 01:09:09 +0900

    feat: ROUTES 상수 추가

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,15 @@
 import { Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import ROUTES from '@constant/routes';
 import { Link } from 'expo-router';
 
 const IndexPage = () => (
   <SafeAreaView>
+    <Link href={ROUTES.KANA.HIRAGANA}>
+      <Text>히라가나/가타카나</Text>
+    </Link>
     <Link href="/hiragana">
-      <Text>히라가나 페이지</Text>
+      <Text>히라가나</Text>
     </Link>
     <Link href="/word">
       <Text>단어장 페이지</Text>
@@ -19,7 +23,7 @@ const IndexPage = () => (
     <Link href="/soundWrite">
       <Text>사운드 퀴즈 페이지(텍스트)</Text>
     </Link>
-    <Link href="/setting">
+    <Link href={ROUTES.SETTING}>
       <Text>공지사항</Text>
     </Link>
   </SafeAreaView>
