b0c80d8b54444ed39ad066ba4beb0ebf7d3dc5b2
Kim Ji Yu <spbabo97@naver.com>
2024-12-25 01:38:43 +0900

    feat : 단어장 무한 스크롤

--- service/getWords.ts
+++ service/getWords.ts
@@ -4,11 +4,12 @@ import { supabase } from 'lib/supabase';
  * 단어장을 가져옵니다.
  * @returns 단어장배열 반환
  */
-const getWords = async () => {
+const getWords = async (page: number, pageSize: number) => {
   const { data, error } = await supabase
     .from('words')
-    .select()
-    .order('id', { ascending: false });
+    .select('*')
+    .order('id', { ascending: false })
+    .range((page - 1) * pageSize, page * pageSize - 1);
 
   if (error) {
     throw new Error(error.message);
