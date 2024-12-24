4d6e20eddc97116271c5cd4e4474378d8290cb9e
Kim Ji Yu <spbabo97@naver.com>
2024-12-25 01:05:06 +0900

    feat : 단어장 페이지 탭 구현

--- screens/(word)/WordScreen.tsx
+++ screens/(word)/WordScreen.tsx
@@ -1,25 +1,52 @@
-import { View, StyleSheet } from 'react-native';
-import React, { lazy, Suspense } from 'react';
+import { View, StyleSheet, useWindowDimensions } from 'react-native';
+import { SceneMap, TabView } from 'react-native-tab-view';
+import React, { lazy, Suspense, useState } from 'react';
 import Loading from '@components/common/Loading';
 import Header from '@components/common/layout/Header';
 import PrevBtn from '@components/common/layout/Header/subcomponents/PrevBtn';
 import SettingBtn from '@components/common/layout/Header/subcomponents/SettingBtn';
+import BookMarkTabContent from '@components/page/(word)/TabContents/BookMarkTabContent';
+import KanaMenuTabBar from '@components/page/kana/KanaMenuTabBar';
+import MinigameTabContent from '@components/page/kana/TabContents/MinigameTabContent';
 
-const WordList = lazy(() => import('@components/page/(word)/WordList'));
+const WordListTabContent = lazy(
+  () => import('@components/page/(word)/TabContents/WordListTabContent'),
+);
+
+/* routes 요거도 나중에 따로 상수처리하거나 할듯 */
+const routes = [
+  { key: 'word', title: '단어장' },
+  { key: 'bookmark', title: '북마크' },
+  { key: 'miniGame', title: '미니게임' },
+];
+
+const renderScene = SceneMap({
+  word: WordListTabContent,
+  bookmark: BookMarkTabContent,
+  miniGame: MinigameTabContent,
+});
 
 const WordScreen = () => {
+  const layout = useWindowDimensions();
+  const [tabIndex, setTabIndex] = useState(0);
   return (
     <View style={s.container}>
       <Header>
-        <Header.left>
+        <Header.Left>
           <PrevBtn />
-        </Header.left>
-        <Header.right>
+        </Header.Left>
+        <Header.Right>
           <SettingBtn />
-        </Header.right>
+        </Header.Right>
       </Header>
       <Suspense fallback={<Loading />}>
-        <WordList />
+        <TabView
+          navigationState={{ index: tabIndex, routes }}
+          renderScene={renderScene}
+          onIndexChange={setTabIndex}
+          initialLayout={{ width: layout.width }}
+          renderTabBar={KanaMenuTabBar}
+        />
       </Suspense>
     </View>
   );
