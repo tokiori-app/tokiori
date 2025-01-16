036f963f91a70a8bf32bb6f97606e0797b412983
Kim Ji Yu <spbabo97@naver.com>
2025-01-16 22:50:44 +0900

    fix : supabase 권장으로 수정

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,17 @@
+import 'react-native-url-polyfill/auto';
+import { Database } from '$types/database';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
-import { Database } from 'types/database';
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL ?? '';
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY ?? '';
-
-export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient<Database>(
+  process.env.EXPO_PUBLIC_SUPABASE_URL ?? '',
+  process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY ?? '',
+  {
+    auth: {
+      storage: AsyncStorage,
+      autoRefreshToken: true,
+      persistSession: true,
+      detectSessionInUrl: false,
+    },
+  },
+);
